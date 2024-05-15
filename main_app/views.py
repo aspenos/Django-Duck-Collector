911f8dc6e5e90f54d25d836e7ef1801b7c7d9ecc
aspenos <arisspenos1@gmail.com>
2024-05-15 19:12:27 -0400

    Code the views

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import Duck
+from .serializers import DuckSerializer
 
 # Create your views here.
+
+class DuckList(generics.ListCreateAPIView):
+    queryset = Duck.objects.all()
+    serializer_class = DuckSerializer
+
+class DuckDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Duck.objects.all()
+    serializer_class = DuckSerializer
+    lookup_field = 'id'
