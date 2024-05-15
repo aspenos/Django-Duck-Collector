4830d067835aa98ca14c1cc2ed8c309309c52825
aspenos <arisspenos1@gmail.com>
2024-05-15 19:02:38 -0400

    Define routes in urls.py

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 # Add the include function to the import
 from django.urls import path, include
-from .views import Home
+from .views import Home, DuckList, DuckDetail #additional imports
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', Home.as_view(), name='home'),
     # '' represents the "starts with" path
-    path('', include('main_app.urls'))
+    path('ducks/', DuckList.as_view(), name='duck_list'),
+    path('ducks/<int:id>/', DuckDetail.as_view(), name='duck_detail'),
 ]
